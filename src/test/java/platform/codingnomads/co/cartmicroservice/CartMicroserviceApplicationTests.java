5b45b352c8e58f9d134afcc45e5f781e9c94e0e1
sam <sam.leefr@gmail.com>
2023-10-04 23:12:59 +0200

    Initial commit

---
+++ src/test/java/platform/codingnomads/co/cartmicroservice/CartMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package platform.codingnomads.co.cartmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
